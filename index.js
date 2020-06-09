122dfae28ec5e63b747e39ddcb09d627adc168d3
Alex Fernández <alexfernandeznpm@gmail.com>
2020-06-09 21:00:07 +0200

    Converted to a class.

--- index.js
+++ index.js
@@ -10,57 +10,62 @@ const options = getopt({
     series: {key: 's', args: 1, description: 'Consecutive requests', default: 1},
 })
 
-const samples = computeSamples()
-showPercentiles(samples)
-
-function computeSamples() {
-	let sum = 0
-	let min = Infinity
-	let max = 0
-	const samples = []
-	for (let i = 0; i < options.number; i++) {
-		const sample = computeSample()
-		samples.push(sample)
-		sum += sample
-		if (sample > max) max = sample
-		if (sample < min) min = sample
+class Simulation {
+	constructor() {
+		this.samples = []
+		this.sum = 0
+		this.min = Infinity
+		this.max = 0
 	}
-	const average = sum / options.number
-	console.log(`Pareto distribution with xm=${options.xm} and alpha=${options.alpha}`)
-	console.log(`Server requests: ${options.series} calls in parallel, ${options.parallel} in parallel`)
-	console.log(`Average: ${average.toFixed(2)}, min: ${min.toFixed(2)}, max: ${max.toFixed(2)}`)
-	return samples
-}
 
-function computeSample() {
-	if (options.parallel == 1 && options.series == 1) {
-		return computePareto()
-	}
-	let sum = 0
-	for (let i = 0; i < options.series; i++) {
-		let max = 0
-		for (let j = 0; j < options.parallel; j++) {
-			const sample = computePareto()
-			if (sample > max) max = sample
+	computeSamples() {
+		for (let i = 0; i < options.number; i++) {
+			const sample = this.computeSample()
+			this.samples.push(sample)
+			this.sum += sample
+			if (sample > this.max) this.max = sample
+			if (sample < this.min) this.min = sample
 		}
-		sum += max
 	}
-	return sum
+
+	computeSample() {
+		if (options.parallel == 1 && options.series == 1) {
+			return this.computePareto()
+		}
+		let sum = 0
+		for (let i = 0; i < options.series; i++) {
+			let max = 0
+			for (let j = 0; j < options.parallel; j++) {
+				const sample = this.computePareto()
+				if (sample > max) max = sample
+			}
+			sum += max
+		}
+		return sum
+	}
+
+	computePareto() {
+		const random = Math.random()
+		const sample = options.xm / (random ** (1 / options.alpha))
+		if (!options.timeout || sample <= options.timeout) return sample
+		return parseFloat(options.timeout) + this.computePareto()
+	}
+
+	showStats() {
+		const percentiles = {}
+		this.samples.sort((a, b) => a - b)
+		for (const percent of percents) {
+			percentiles[percent] = this.samples[options.number * percent / 100].toFixed(2)
+		}
+		const average = this.sum / options.number
+		console.log(`Pareto distribution with xm=${options.xm} and alpha=${options.alpha}`)
+		console.log(`Server requests: ${options.series} calls in parallel, ${options.parallel} in parallel`)
+		console.log(`Average: ${average.toFixed(2)}, min: ${this.min.toFixed(2)}, max: ${this.max.toFixed(2)}`)
+		console.log(`Percentiles: ${JSON.stringify(percentiles, null, '\t')}`)
+	}
 }
 
-function computePareto() {
-	const random = Math.random()
-	const sample = options.xm / (random ** (1 / options.alpha))
-	if (!options.timeout || sample <= options.timeout) return sample
-	return parseFloat(options.timeout) + computePareto()
-}
-
-function showPercentiles(samples) {
-	const percentiles = {}
-	samples.sort((a, b) => a - b)
-	for (const percent of percents) {
-		percentiles[percent] = samples[options.number * percent / 100].toFixed(2)
-	}
-	console.log(`Percentiles: ${JSON.stringify(percentiles, null, '\t')}`)
-}
+const simulation = new Simulation()
+simulation.computeSamples()
+simulation.showStats()
 
